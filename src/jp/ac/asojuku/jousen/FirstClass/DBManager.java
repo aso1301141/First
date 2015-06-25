8513b6fcca127cbad07bbfd7186619ecf124bcb7
student <student@PC634S07.asost.local>
2015-06-25 10:38:42 +0900

    DB構築

--- src/jp/ac/asojuku/jousen/FirstClass/DBManager.java
+++ src/jp/ac/asojuku/jousen/FirstClass/DBManager.java
@@ -1,11 +1,10 @@
 /**
- * 
+ *
  */
 package jp.ac.asojuku.jousen.FirstClass;
 
 import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
-import android.database.sqlite.SQLiteDatabase.CursorFactory;
 import android.database.sqlite.SQLiteOpenHelper;
 
 /**
@@ -14,9 +13,8 @@ import android.database.sqlite.SQLiteOpenHelper;
  */
 public class DBManager extends SQLiteOpenHelper {
 
-	public DBManager(Context context, String name, CursorFactory factory,
-			int version) {
-		super(context, name, factory, version);
+	public DBManager(Context context) {
+		super(context, "FirstClass.sqlite3", null, 1);
 		// TODO 自動生成されたコンストラクター・スタブ
 	}
 
@@ -27,6 +25,17 @@ public class DBManager extends SQLiteOpenHelper {
 	public void onCreate(SQLiteDatabase db) {
 		// TODO 自動生成されたメソッド・スタブ
 
+		String sql1="CREATE TABLE member(mem_num text PRIMARY KEY,mem_name text,mail text,pass text,sex text)";
+		String sql2="CREATE TABLE con(con_num text PRIMARY KEY,mem_num text,title text,des text,pic_pass text)";
+		String sql3="CREATE TABLE con_his(mem_num text PRIMARY KEY ,con_num text)";
+		String sql4="CREATE TABLE favorite(mem_num text PRIMARY KEY , fav_mem text)";
+		String sql5="CREATE TABLE con_det(con_num text PRIMARY KEY , com_mem text,comment text)";
+
+		db.execSQL(sql1);
+		db.execSQL(sql2);
+		db.execSQL(sql3);
+		db.execSQL(sql4);
+		db.execSQL(sql5);
 	}
 
 	/* (非 Javadoc)
@@ -35,7 +44,13 @@ public class DBManager extends SQLiteOpenHelper {
 	@Override
 	public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
 		// TODO 自動生成されたメソッド・スタブ
+			db.execSQL("DROP TABLE member");
+			db.execSQL("DROP TABLE con");
+			db.execSQL("DROP TABLE con_his");
+			db.execSQL("DROP TABLE favorite");
+			db.execSQL("DROP TABLE con_dat");
 
+			onCreate(db);
 	}
 
 }
